d1582027ee2b5b352620b03c5f6c584cb2ad3e17
thaopm <thaopm.neu@gmail.com>
2024-03-12 21:22:26 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,9 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}<p>ThaoPM</p></h1>;
+  return <>
+  <h1>Slug: {params.slug}</h1>
+  <p>ThaoPM</p>
+  <p>Welcome to Hanoi</p>
+  </>
 }
